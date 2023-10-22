bff9f686d8988597ad1d7ee0d432afd44baaa8ff
adgianv <a.digianvito@hotmail.it>
2023-10-22 15:09:57 +0200

    primary file, first one with MSE

--- friday_7pm.py
+++ friday_7pm.py
@@ -99,12 +99,14 @@ def winsorize(data, limits=(0.05, 0.05)):
 # Windsorizing the price variable
 df['price'] = winsorize(df['price'], limits=(0.05, 0.05))
 
-
 # Replacing the outliers with NaN in the number of rooms (justify cutoff value: outliers are very high above 10)
 df['num_rooms'] = df['num_rooms'].apply(lambda x: x if x<4 else np.nan)
 
 # Replacing the values of square metres < 40 with NaN (change the cutoff value and see the results)
 df.loc[df['square_meters'] < 0, 'square_meters'] = np.nan
+
+df['floor_one_dummy'] = df['floor'].apply(lambda x: True if x==1 else False)
+
 
 ###################
 # Standardization #
@@ -132,7 +134,6 @@ for i in to_standardize:
 
 #     return normalized_data
 
-
 # for col in to_normalize:
 #     df[col] = normalize_data(df[col])
 
@@ -145,14 +146,6 @@ for i in to_standardize:
 
 # Dropping NaNs from year built (justify: difficult to predict based on other variables, low value)
 df = df.dropna(subset=['year_built'])
-# df['year_built'] = df['year_built'].fillna(df['year_built'].mean())
-
-
-# Checking number of rows with multiple nulls (some examples)
-
-# print(df[(df['num_baths'].isnull()) & (df['square_meters'].isnull())]) #8#
-# print(df[(df['num_crimes'].isnull()) & (df['square_meters'].isnull())]) #4#
-# print(df[(df['floor'].isnull()) & (df['square_meters'].isnull())]) #2#
 
 # dropping the rows that have multiple missing values
 cols = ['num_rooms', 'num_baths', 'square_meters', 'year_built', 'floor', 'num_crimes']
@@ -163,13 +156,12 @@ for i in cols:
         if i != j:
             df = df[(df[i].notnull()) | (df[j].notnull())]
 
-
 #####################
 # Imputing with KNN #
 #####################
 knn_cols = ['num_rooms', 'num_baths', 'square_meters', 'floor', 'num_crimes', 'price']
 df_sub = df[knn_cols]
-imputer = KNNImputer(n_neighbors=3)
+imputer = KNNImputer(n_neighbors=7)
 imputed_data = imputer.fit_transform(df_sub)
 df_sub = pd.DataFrame(imputed_data, columns=df_sub.columns)
 # print(round((df_sub.isnull().sum() / len(df_sub) * 100), 2))
@@ -187,122 +179,99 @@ df = df.dropna(axis= 0)
 # Categorical Variable Encoding # 
 #################################
 
-# Using price to order the neighborhoods by mean price
+# Using crime to order the neighborhoods by mean num_crimes
 # sns.boxplot(data=df, x='neighborhood', y='num_crimes')
-
-# # Encoding neighborhood by mean price of house
-# neighb_mean_price = df.groupby('neighborhood')['price'].mean()
-# df['neighborhood_price_encoded'] = df['neighborhood'].map(neighb_mean_price)
 
 neighb_mean_crime = df.groupby('neighborhood')['num_crimes'].mean()
 df['neighborhood_crime_encoded'] = df['neighborhood'].map(neighb_mean_crime)
-
-# def fill_crimes(row):
-#     if pd.notna(row['num_crimes']):
-#         return row['num_crimes']
-#     neighborhood = row['neighborhood']
-#     if neighborhood in neighb_mean_crime:
-#         return neighb_mean_crime[neighborhood]
-#     else:
-#         return None
-# df['num_crimes'] = df.apply(fill_crimes, axis=1)
 
 ########################
 # Feature Engiineering # 
 ########################
 
-# **NOT SURE IF SHOULD BE DONE BEFORE STANDARDIZING**
-
-# # Floor looks to have a cubed distribution, so cubing the floor variable 8the coefficient grows by a lot (1.52 to 6.36) but the score slightly decreases)
-# df['floor'] = df['floor'] ** 3
-
-#IDEAS:
-# Rooms per square meter
-# Bathrooms per num_rooms
-
-# df['rooms_per_bath'] = df['num_rooms']/df['num_baths']
+df['rooms_per_bath'] = df['num_rooms']/df['num_baths']
 
 ##################
 # Splitting data #
 ##################
-features = ['num_rooms', 'num_baths', 'square_meters', 'floor', 'num_crimes', 'neighborhood_crime_encoded','has_ac']
+
+features = ['num_rooms', 'num_baths', 'square_meters', 'floor', 'num_crimes', 'neighborhood_crime_encoded','has_ac', 'floor_one_dummy', 'rooms_per_bath']
 target = ['price']
-# X_train, X_test, y_train, y_test = train_test_split(df[features], df[target], test_size= 0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(df[features], df[target], test_size= 0.2, random_state=42)
 
 ####################
 # Linear Modelling #
 ####################
 
-# Running simple linear model without feature scaling, using  num_crimes and square_meters as predictors
+# Running simple linear model
 model = LinearRegression()
-model.fit(df[features], df[target])
+model.fit(X_train, y_train)
+# model.fit(df[features], df[target])
 
-# # Model res
-# model.coef_
-# model.intercept_
-# model.score(df[features], df[target])
-
-##############
-# Prediction #
-##############
-
-# y_pred = model.predict(X_test)
-# r_squared = r2_score(y_test, y_pred)
-# mse = mean_squared_error(y_test, y_pred)
-
-# print(r_squared)
-# print(mse)
-
+# Model res
+model.score(df[features], df[target])
 
 ###########
 # Testing #
 ###########
 
-# Test data import
-data_path_test = "./data//test.csv"
-df_test = pd.read_csv(data_path_test)
-df_test=df_test.sort_values("id").reset_index(drop=True)
+y_pred = model.predict(X_test)
+r_squared = r2_score(y_test, y_pred)
+mse = mean_squared_error(y_test, y_pred)
 
-# Dropping columns with high missing percentage
-df_test.drop('num_supermarkets', axis=1, inplace=True)
-df_test.drop('orientation', axis=1, inplace=True)
+print(r_squared)
+print(mse)
 
-# Creating floor variable
-df_test[['floor', 'door_num']] = df_test['door'].str.split('-', n=1, expand=True)
-df_test['floor'] = df_test['floor'].str[0]
-df_test["floor"] = pd.to_numeric(df_test["floor"])
 
-# Dropping door and door_num columns
-df_test.drop('door', axis=1, inplace=True)
-df_test.drop('door_num', axis=1, inplace=True)
+##############
+# Submitting #
+##############
 
-# Turning to nan sq.mt and num_rooms
-df_test['num_rooms'] = df_test['num_rooms'].apply(lambda x: x if x<4 else np.nan)
-df_test.loc[df_test['square_meters'] < 0, 'square_meters'] = np.nan
+# # Test data import
+# data_path_test = "./data//test.csv"
+# df_test = pd.read_csv(data_path_test)
+# df_test=df_test.sort_values("id").reset_index(drop=True)
 
-# Neighborhood encoding and dropping categorical variable
-neighb_mean_crime = df_test.groupby('neighborhood')['num_crimes'].mean()
-df_test['neighborhood_crime_encoded'] = df_test['neighborhood'].map(neighb_mean_crime)
-df_test.drop('neighborhood', axis=1, inplace=True)
+# # Dropping columns with high missing percentage
+# df_test.drop('num_supermarkets', axis=1, inplace=True)
+# df_test.drop('orientation', axis=1, inplace=True)
 
-# Standardizing
-to_standardize = ['num_rooms', 'num_baths', 'square_meters', 'year_built', 'floor', 'num_crimes', 'neighborhood_crime_encoded']
-for i in to_standardize:
-    df_test[i] = (df_test[i] - np.mean(df_test[i])) / np.std(df_test[i])
+# # Creating floor variable
+# df_test[['floor', 'door_num']] = df_test['door'].str.split('-', n=1, expand=True)
+# df_test['floor'] = df_test['floor'].str[0]
+# df_test["floor"] = pd.to_numeric(df_test["floor"])
 
-df = df.dropna(subset=['year_built'])
-# KNN 
-imputer = KNNImputer(n_neighbors=3)
-imputed_data = imputer.fit_transform(df_test)
-df_test = pd.DataFrame(imputed_data, columns=df_test.columns)
-print(round((df_test.isnull().sum() / len(df_test) * 100), 2))
+# # Dropping door and door_num columns
+# df_test.drop('door', axis=1, inplace=True)
+# df_test.drop('door_num', axis=1, inplace=True)
 
-# Prediction
-x_test = df_test[features]
-y_pred = model.predict(x_test)
+# # Turning to nan sq.mt and num_rooms
+# df_test['num_rooms'] = df_test['num_rooms'].apply(lambda x: x if x<4 else np.nan)
+# df_test.loc[df_test['square_meters'] < 0, 'square_meters'] = np.nan
 
-df_pred = pd.DataFrame()
-df_pred['id'] = df_test['id']
-df_pred['price'] = y_pred
+# # Neighborhood encoding and dropping categorical variable
+# neighb_mean_crime = df_test.groupby('neighborhood')['num_crimes'].mean()
+# df_test['neighborhood_crime_encoded'] = df_test['neighborhood'].map(neighb_mean_crime)
+# df_test.drop('neighborhood', axis=1, inplace=True)
 
-df_pred.to_csv('./prediction_friday7_30pm.csv', index=False)
+# # Standardizing
+# to_standardize = ['num_rooms', 'num_baths', 'square_meters', 'year_built', 'floor', 'num_crimes', 'neighborhood_crime_encoded']
+# for i in to_standardize:
+#     df_test[i] = (df_test[i] - np.mean(df_test[i])) / np.std(df_test[i])
+
+# df = df.dropna(subset=['year_built'])
+# # KNN 
+# imputer = KNNImputer(n_neighbors=3)
+# imputed_data = imputer.fit_transform(df_test)
+# df_test = pd.DataFrame(imputed_data, columns=df_test.columns)
+# print(round((df_test.isnull().sum() / len(df_test) * 100), 2))
+
+# # Prediction
+# x_test = df_test[features]
+# y_pred = model.predict(x_test)
+
+# df_pred = pd.DataFrame()
+# df_pred['id'] = df_test['id']
+# df_pred['price'] = y_pred
+
+# df_pred.to_csv('./prediction_friday7_30pm.csv', index=False)
